ca3d24783185506c5519b81b70808311b7b63b1c
avinash <avinashmishracode23@gmail.com>
2026-08-19 21:44:53 +0530

    complete login system in backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signupUser = async (req, res) => {
   try{
@@ -26,3 +27,42 @@ export const signupUser = async (req, res) => {
      res.status(500).json({ message: "server error", error});
   }
 };
+
+// Login  User
+
+export const loginUser = async (req, res) => {
+   try{
+      const { email, password } = req.body;
+
+      // Catch if user already exists
+      const user = await User.findOne({ email });
+      if(!user){
+         return res.status(400).json({ message: "user not found" });
+      }
+
+      // compare password
+      const match = await bcrypt.compare(password, user.password);
+      if(!match) {
+         return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      // Genrate jwt token 
+       const token = jwt.sign(
+         {id: user._id},
+         process.env.JWT_SECRET,
+         { expiresIn: "5d"}
+       );
+       res.json({
+         message: "Login successful",
+         token,
+         user: {
+            id: user._id,
+            name: user.name, 
+            email: user.email
+         }
+       });
+       
+   } catch (error){
+     res.status(500).json({ message: "Server error", error});
+   }
+}
