f0041dc423d96793e9461aa71fc1f3c6235b83e7
avinash <avinashmishracode23@gmail.com>
2026-08-20 15:11:38 +0530

    cart system backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import cartRouter from './routes/cartRouter.js';
 
 dotenv.config();
 
@@ -13,12 +14,15 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use('/cart', cartRouter);
 
 app.get('/', (req, res) => {
     res.send('api is runnig...');
 });
 
+
 connectDB();
+
 
 app.listen(5001, () => {
     console.log(`server is start on port 5001`);
