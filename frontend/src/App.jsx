1ab9a57f2859e3d45370c0d66021d666b1a4fccf
avinash <avinashmishracode23@gmail.com>
2026-08-20 14:05:39 +0530

    create admin managment frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProductDeails from "./pages/ProductDetails";
+import AddProduct from "./admin/AddProduct";
+import EditProduct from "./admin/EditProduct";
+import ProductList from "./admin/ProductList";
 
 
 const router = createBrowserRouter([
@@ -10,6 +13,10 @@ const router = createBrowserRouter([
   {path: "/login", element: <Login/>},
   {path: "/signup", element: <Signup/>},
   {path: "/product/:id", element: <ProductDeails/>},
+
+  {path: "/admin/Products", element: <ProductList/>},
+  {path: "/admin/products/add", element: <AddProduct/>},
+  {path: "/admin/products/:id", element: <EditProduct/>},
 ]);
 
 export default function App() {
