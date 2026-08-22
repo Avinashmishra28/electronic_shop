8daf6980ba552d22e82178c0f76bfbe166a61a4f
avinash <avinashmishracode23@gmail.com>
2026-08-22 11:06:18 +0530

    adress checkout backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cartRouter from './routes/cartRouter.js';
+import addressRouter from './routes/addressRouter.js';
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
-app.use('/cart', cartRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/address', addressRouter);
 
 app.get('/', (req, res) => {
     res.send('api is runnig...');
