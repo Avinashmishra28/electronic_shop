970db934b59c63de86e4107a698de20d70ffe04f
avinash <avinashmishracode23@gmail.com>
2026-08-23 11:57:13 +0530

    change env

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,8 @@ app.get('/', (req, res) => {
 
 connectDB();
 
-
-app.listen(5001, () => {
-    console.log(`server is start on port 5001`);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
     
 });
