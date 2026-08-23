e726eae1c9dc01fd00b0e431bcba8b927d07c421
avinash <avinashmishracode23@gmail.com>
2026-08-23 11:32:42 +0530

    fix in backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,22 +9,29 @@ export const signupUser = async (req, res) => {
        // Check if user already exists
        const userExist = await User.findOne({ email });
        if(userExist) {
-        return res.status(400).json({ message: "User alerdy registered" });
+        return res.status(400).json({ 
+         message: "User alerdy registered" 
+      });
        }
 
        // Hash password
         const hashPassword = await bcrypt.hash(password, 10);
 
         //create User
-         await User.create({
+        const user =  await User.create({
             name,
             email, 
             password: hashPassword
          });
 
-           res.json({ message: "User registerd successfully" });
+           res.status(201).json({
+             message: "User registerd successfully",
+            userId: user._id
+            });
   } catch (error) {
-     res.status(500).json({ message: "server error", error});
+     res.status(500).json({
+       message: "server error", error
+      });
   }
 };
 
